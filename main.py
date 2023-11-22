9bc5912f74cd0df906ea20547576e09938f77df2
Rodrigo Amaral Dias Soares <rodrigoadiassoares@gmail.com>
2023-11-22 16:04:52 -0300

    feat: Add simple menu and option to download one video

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 import yt
 
-link = input("Cole o link da playlist: ")
-print("Downloading...")
-yt.download_playlist(link)
-print("Finalizado.")
+print('''
+  Baixar videos do YT
+  Selecione uma das opções abaixo:
+  
+  1 - Baixar uma playlist
+  2 - Baixar um vídeo
+''')
 
+opcao = int(input("Opção: "))
+
+if opcao == 1:
+  link = input("Cole o link da playlist: ")
+  print("Downloading...")
+  yt.download_playlist(link)
+  print("Finalizado.")
+
+elif opcao == 2:
+  link = input("Cole o link do video: ")
+  print("Downloading...")
+  yt.download_video(link)
+  print("Finalizado.")
+
+else:
+  print("Opção inválida.")
--- yt.py
+++ yt.py
@@ -1,10 +1,13 @@
 import pytube
 
-def download_playlist(url):
-  playlist = pytube.Playlist(url)
+def download_playlist(playlist_url):
+  playlist = pytube.Playlist(playlist_url)
   for url in playlist.video_urls:
-    resolution = 22
-    video = pytube.YouTube(url)
-    stream = video.streams.get_by_itag(resolution)
-    stream.download()
-    return stream.default_filename
+    download_video(url)
+  
+def download_video(url):
+  resolution = 22 # 720p
+  video = pytube.YouTube(url)
+  stream = video.streams.get_by_itag(resolution)
+  stream.download('downloads')
+  return stream.default_filename
